05e9df8144d695860bc340ab148e91120e6b689d
Albanlv <albanlv@gmail.com>
2013-01-15 15:37:27 +0100

    overriding default after sign in path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :account_url
+
+ def account_url
+    return new_user_session_url unless user_signed_in?
+    case current_user.class.name
+    when 'Customer'
+      edit_customer_registration_url
+    when 'Admin'
+      edit_home_page_section_url
+    else
+      root_url
+    end if user_signed_in?
+  end
 end
+
